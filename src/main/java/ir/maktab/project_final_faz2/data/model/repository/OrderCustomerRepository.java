b9f7213905fc7acd86f439954727f01cf93f45d5
somaye6868 <somayekariminia6868@gmail.com>
2023-01-23 07:44:28 +0800

    add methode update to OrderCustmerrepository

--- src/main/java/ir/maktab/project_final_faz2/data/model/repository/OrderCustomerRepository.java
+++ src/main/java/ir/maktab/project_final_faz2/data/model/repository/OrderCustomerRepository.java
@@ -1,2 +1,19 @@
-package ir.maktab.project_final_faz2.data.model.repository;public interface OrderRepository {
+package ir.maktab.project_final_faz2.data.model.repository;
+
+import ir.maktab.project_final_faz2.data.model.entity.OrderCustomer;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface OrderCustomerRepository extends JpaRepository<OrderCustomer,Long> {
+
+    @Query("select  o from OrderCustomer o where (o.subJob.id=: id) order by o.offerPrice asc ")
+    List<OrderCustomer> findAllBySubJobForAExpert(@Param("id") Long id);
+    @Modifying
+    @Query("update OrderCustomer o set  o.orderStatus=?1 where o.id =? 2")
+    void update(@Param("orderStatues") String orderStatus,Long id);
 }
